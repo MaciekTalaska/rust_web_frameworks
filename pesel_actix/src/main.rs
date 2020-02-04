11fc60544ef0bc01b62e6212b4937f4c755df9a2
MaciekTalaska <maciek.talaska@gmail.com>
2020-02-04 16:21:51 +0100

    actix-web: pesel validator added

--- pesel_actix/src/main.rs
+++ pesel_actix/src/main.rs
@@ -1,10 +1,19 @@
 use actix_web::{get, error, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::str::FromStr;
 
 #[get("/hi")]
 async fn hi() -> impl Responder {
     format!("Hello world from actix-web")
 }
 
+#[get("/pesel/{pesel}")]
+async fn pesel_check(info: web::Path<(String)>) -> impl Responder {
+    let result = pesel::pesel::PESEL::from_str(&info);
+    match result {
+        Ok(pesel) => format!("{}", pesel),
+        _ => format!("{} is not a valid pesel number", &info)
+    }
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -12,6 +21,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(||
         App::new()
             .service(hi)
+            .service(pesel_check)
     )
         .bind("127.0.0.1:8080")?
         .run()
