e26ed2abfad8832db4ae0f448aa9c9af1654a7a5
MaciekTalaska <maciek.talaska@gmail.com>
2020-02-04 16:37:07 +0100

    actix-web: pesel creation added

--- pesel_actix/src/main.rs
+++ pesel_actix/src/main.rs
@@ -7,12 +7,23 @@ async fn hi() -> impl Responder {
 }
 
 #[get("/pesel/{pesel}")]
-async fn pesel_check(info: web::Path<(String)>) -> impl Responder {
+async fn pesel_check(info: web::Path<String>) -> impl Responder {
     let result = pesel::pesel::PESEL::from_str(&info);
     match result {
         Ok(pesel) => format!("{}", pesel),
         _ => format!("{} is not a valid pesel number", &info)
     }
+}
+
+#[get("/pesel/{year}/{month}/{day}/{gender}")]
+async fn pesel_creation(info: web::Path<(u16, u8, u8, String)>) -> impl Responder {
+    let gender = info.3.as_str();
+    let biological_gender = match gender {
+        "m" => pesel::pesel::PeselGender::Male,
+        _ => pesel::pesel::PeselGender::Female,
+    };
+    let generated_pesel = pesel::pesel::PESEL::new(info.0, info.1, info.2, biological_gender);
+    format!("{}", generated_pesel)
 }
 
 #[actix_rt::main]
@@ -22,6 +33,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hi)
             .service(pesel_check)
+            .service( pesel_creation)
     )
         .bind("127.0.0.1:8080")?
         .run()
