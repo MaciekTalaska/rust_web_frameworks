ef6a267eb48a50ffcd81c4c1c36b6dfb3689aa91
MaciekTalaska <maciek.talaska@gmail.com>
2020-02-06 17:55:20 +0100

    warp: pesel creation added

--- pesel-warp/src/main.rs
+++ pesel-warp/src/main.rs
@@ -9,15 +9,33 @@ pub fn validate_pesel(pesel_number: &str) -> String {
     }
 }
 
+pub fn generate_pesel(year: u16, month: u8, day: u8, gender: String) -> pesel::pesel::PESEL {
+    let biological_gender = match gender.as_str() {
+        "m" => pesel::pesel::PeselGender::Male,
+        _ => pesel::pesel::PeselGender::Female
+    };
+
+    pesel::pesel::PESEL::new(year, month, day, biological_gender)
+}
+
 #[tokio::main]
 async fn main() {
     let hi = warp::path!("hi")
         .map(|| format!("Hello World from warp!"));
 
     let pesel_validator = warp::path!("pesel" / String)
-        .map(|pesel: String| format!("argument was: {}", validate_pesel(pesel.as_str())));
+        .map(|pesel: String| format!("{}", validate_pesel(pesel.as_str())));
 
-    let routes = warp::get().and(hi.or(pesel_validator));
+    let pesel_generator = warp::path!("pesel" / u16 / u8 / u8 / String)
+        .map(|year, month, day, gender| format!("{}", generate_pesel(year, month, day, gender )));
+
+    let routes = warp::get().
+        and(hi
+            .or(pesel_validator)
+            .or(pesel_generator)
+        );
+
+    println!("warp listening on: 127.0.0.1:8080");
     warp::serve(routes)
         .run(([127, 0, 0, 1], 8080))
         .await;
