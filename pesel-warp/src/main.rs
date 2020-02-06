c627998d927e2337d1a6ca697ba79d11e57f4ee5
MaciekTalaska <maciek.talaska@gmail.com>
2020-02-06 17:34:36 +0100

    warp: pesel validation added

--- pesel-warp/src/main.rs
+++ pesel-warp/src/main.rs
@@ -1,11 +1,24 @@
 use warp::Filter;
+use std::str::FromStr;
+
+pub fn validate_pesel(pesel_number: &str) -> String {
+    let p = pesel::pesel::PESEL::from_str(pesel_number);
+    match p {
+        Ok(pesel) => format!("{}",pesel),
+        _ => format!("invalid pesel")
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let hi = warp::path!("hi")
         .map(|| format!("Hello World from warp!"));
 
-    warp::serve(hi)
+    let pesel_validator = warp::path!("pesel" / String)
+        .map(|pesel: String| format!("argument was: {}", validate_pesel(pesel.as_str())));
+
+    let routes = warp::get().and(hi.or(pesel_validator));
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 8080))
         .await;
 }
