05c356697df4db468dfcef0e79eb11d14ce0ed27
MaciekTalaska <maciek.talaska@gmail.com>
2020-03-27 19:51:19 +0100

    both routes now return proper JSON

--- pesel-warp/src/main.rs
+++ pesel-warp/src/main.rs
@@ -1,33 +1,69 @@
 use warp::Filter;
 use std::str::FromStr;
 
-pub fn validate_pesel(pesel_number: &str) -> String {
-    let p = pesel::pesel::PESEL::from_str(pesel_number);
+use pesel::pesel::PESEL;
+use pesel::pesel::PeselGender;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize)]
+struct PESELInfo {
+    pesel_number: String,
+    date_of_birth: String,
+    gender: String,
+    is_valid: bool,
+}
+
+pub fn validate_pesel(pesel_number: &str) ->  warp::reply::Json {
+    let p = PESEL::from_str(pesel_number);
     match p {
-        Ok(pesel) => format!("{}",pesel),
-        _ => format!("invalid pesel")
+        Ok(pesel) => {
+            let pesel_json = PESELInfo {
+                pesel_number: pesel_number.to_string(),
+                gender: pesel.gender_name(),
+                date_of_birth: pesel.date_of_birth(),
+                is_valid: pesel.is_valid()
+            };
+
+            warp::reply::json(&pesel_json)
+        }
+
+        _ => warp::reply::json(&"invalid pesel")
     }
 }
 
-pub fn generate_pesel(year: u16, month: u8, day: u8, gender: String) -> pesel::pesel::PESEL {
+pub fn generate_pesel(year: u16, month: u8, day: u8, gender: String) -> warp::reply::Json {
     let biological_gender = match gender.as_str() {
-        "m" => pesel::pesel::PeselGender::Male,
-        _ => pesel::pesel::PeselGender::Female
+        "m" => PeselGender::Male,
+        _ => PeselGender::Female
     };
 
-    pesel::pesel::PESEL::new(year, month, day, biological_gender)
+    let pesel = PESEL::new(year, month, day, biological_gender);
+    let pesel_json = PESELInfo {
+        pesel_number: "whatever".to_string(), // this will be updated after new version of PESEL is published to crates.io
+        gender: pesel.gender_name(),
+        date_of_birth: pesel.date_of_birth(),
+        is_valid: pesel.is_valid(),
+    };
+    warp::reply::json(&pesel_json)
 }
 
 #[tokio::main]
 async fn main() {
     let hi = warp::path!("hi")
-        .map(|| format!("Hello World from warp!"));
+        .map(|| {
+            warp::http::Response::builder()
+                .header("content-type", "application/json; charset=utf-8");
+                let msg = "Hello World from Warp!";
+                warp::reply::json(&msg)});
 
     let pesel_validator = warp::path!("pesel" / String)
-        .map(|pesel: String| format!("{}", validate_pesel(pesel.as_str())));
+        .map(|pesel: String| {
+           validate_pesel(pesel.as_str())
+        });
 
     let pesel_generator = warp::path!("pesel" / u16 / u8 / u8 / String)
-        .map(|year, month, day, gender| format!("{}", generate_pesel(year, month, day, gender )));
+        .map(|year, month, day, gender| generate_pesel(year, month, day, gender ));
 
     let routes = warp::get().
         and(hi
